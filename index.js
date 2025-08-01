06b64936e5edcb26ec3be275bcb6ed53cf5244c6
MohdShayan <sehbaqureshi@gmail.com>
2025-08-01 10:23:05 +0530

    fix: update CORS origin to correct frontend URL

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173", 
-      "https://your-frontend.vercel.app"
+      "https://chatbot-frontend-nu-lake.vercel.app/"
     ],
     credentials: true,
   })
